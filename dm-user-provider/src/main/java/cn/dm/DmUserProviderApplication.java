1bdc095b60d3428c15aabe300000bb930e27a372
jw1026850952 <66473407+jw1026850952@users.noreply.github.com>
2022-09-28 16:26:23 +0800

    init

---
+++ dm-user-provider/src/main/java/cn/dm/DmUserProviderApplication.java
@@ -0,0 +1,14 @@
+package cn.dm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmUserProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmUserProviderApplication.class, args);
+    }
+//    s
+
+}
